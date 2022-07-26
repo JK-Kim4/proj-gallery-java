bd39c799ba1b3ad456fc295954932d6c555be210
jongwan <jw.kim@changbi.com>
2022-07-26 14:58:22 +0900

    프로젝트명 수정

--- src/main/java/com/gallery/admin/Application.java
+++ src/main/java/com/gallery/admin/Application.java
@@ -1,13 +1,13 @@
-package com.template.springjpa;
+package com.gallery.admin;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class SpringJpaApplication {
+public class Application {
 
     public static void main(String[] args) {
-        SpringApplication.run(SpringJpaApplication.class, args);
+        SpringApplication.run(Application.class, args);
     }
 
 }
